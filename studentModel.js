fd396687ea93f424439e3b584d2c87360b9dc686
982298 <gamifiedlearnig.pvt.ltd@gmail.com>
2026-05-07 12:40:32 +0530

    Add files via upload

---
+++ studentModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema(
+{
+    name: String,
+    email: String,
+    course: String
+},
+{
+    versionKey: false
+});
+
+module.exports = mongoose.model("Student", studentSchema);
